a63165151fb5803be6cc6d1ed52c80e06f2a33be
Peter Ke <peterke0911@gmail.com>
2020-11-07 22:14:48 -0800

    Use async/await

--- pigeon-server/src/controllers/messageController.js
+++ pigeon-server/src/controllers/messageController.js
@@ -15,152 +15,138 @@ const getUserRef = userId => usersRef.doc(userId);
 const stationsRef = db.collection('stations');
 const getStationRef = stationId => stationsRef.doc(stationId);
 
-router.post('/list', (req, res) => {
-    const { userId } = req.body;
-    console.log('POST /messages/list: ', userId);
+router.post('/list', async (req, res) => {
+    try {
 
-    Promise.all([messagesRef.get(), getUserRef(userId).get()])
-        .then(([messagesSnapshot, userSnapshot]) => {
-            const stationId = userSnapshot.get('stationId');
+        const { userId } = req.body;
+        console.log('POST /messages/list: ', userId);
 
-            // Filter to all messages that are either sent or received by user, and map to json
-            const messages = messagesSnapshot.docs
-                .filter(doc => doc.get('sender') === stationId || doc.get('receiver') === stationId)
-                .map(doc => ({
-                    id: doc.id,
-                    sender: doc.get('sender'),
-                    receiver: doc.get('receiver'),
-                    content: doc.get('content'),
-                    time: doc.get('time').valueOf(),
-                    isSent: doc.get('isSent'), // TODO: filter by this too?
-                }));
+        const [messagesSnapshot, userSnapshot] = await Promise.all([messagesRef.get(), getUserRef(userId).get()]);
 
-            res.status(200).json({
-                messages,
-            });
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(500).send(error);
+        const stationId = userSnapshot.get('stationId');
+
+        // Filter to all messages that are either sent or received by user, and map to json
+        const messages = messagesSnapshot.docs
+            .filter(doc => doc.get('sender') === stationId || doc.get('receiver') === stationId)
+            .map(doc => ({
+                id: doc.id,
+                sender: doc.get('sender'),
+                receiver: doc.get('receiver'),
+                content: doc.get('content'),
+                timestamp: doc.get('timestamp').valueOf(),
+                isSent: doc.get('isSent'), // TODO: filter by this too?
+            }));
+
+        res.status(200).json({
+            messages,
         });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error);
+    }
 });
 
-router.post('/prepareSend', (req, res) => {
+/// Prepare sending a message by checking stations, checking distances, etc.
+async function prepareSend(userId, receiver, pigeonId) {
+    const sender = (await getUserRef(userId).get()).get('stationId');
+
+    if (sender === receiver) {
+        throw 'cannot send message to self';
+    }
+
+    const [senderSnapshot, receiverSnapshot] = await Promise.all([
+        getStationRef(sender).get(),
+        getStationRef(receiver).get(),
+    ]);
+
+    if (!senderSnapshot.exists) {
+        throw 'sender station does not exist';
+    } else if (!receiverSnapshot.exists) {
+        throw 'receiver station does not exist';
+    }
+    const senderLocation = senderSnapshot.get('location');
+    const receiverLocation = receiverSnapshot.get('location');
+
+    const senderLatLon = {
+        lat: senderLocation.latitude,
+        lon: senderLocation.longitude,
+    };
+    const receiverLatLon = {
+        lat: receiverLocation.latitude,
+        lon: receiverLocation.longitude,
+    }
+
+    const distance = getDistanceFromLatLonInKm(
+        senderLatLon.lat, senderLatLon.lon,
+        receiverLatLon.lat, receiverLatLon.lon,
+    );
+
+    const duration = distance / PIGEON_SPEED * 60 * 60; // seconds
+
+    return {
+        sender,
+        distance,
+        duration,
+        receiverLocation: receiverLatLon,
+        senderLocation: senderLatLon,
+    };
+}
+
+router.post('/prepareSend', async (req, res) => {
     const { userId, receiver, pigeonId } = req.body;
     console.log('POST prepareSend:', userId, receiver, pigeonId);
 
-    getUserRef(userId).get()
-        .then(userSnapshot => {
-            const sender = userSnapshot.get('stationId');
-            return Promise.all([getStationRef(sender).get(), getStationRef(receiver).get()])
-        })
-        .then(([senderSnapshot, receiverSnapshot]) => {
-            if (!senderSnapshot.exists) {
-                return res.status(400).send('sender station does not exist');
-            } else if (!receiverSnapshot.exists) {
-                return res.status(400).send('receiver station does not exist');
-            }
-            const senderLocation = senderSnapshot.get('location');
-            const receiverLocation = receiverSnapshot.get('location');
+    try {
+        const {
+            distance,
+            duration,
+            senderLocation,
+            receiverLocation,
+        } = await prepareSend(userId, receiver, pigeonId);
 
-            const senderLatLon = {
-                lat: senderLocation.latitude,
-                lon: senderLocation.longitude,
-            };
-            const receiverLatLon = {
-                lat: receiverLocation.latitude,
-                lon: receiverLocation.longitude,
-            }
-
-            const distance = getDistanceFromLatLonInKm(
-                senderLatLon.lat, senderLatLon.lon,
-                receiverLatLon.lat, receiverLatLon.lon,
-            );
-
-            // TODO: get the pigeon speed and subsitute in here
-
-            const time = distance / PIGEON_SPEED; // hours
-
-            res.status(200).send({
-                distance,
-                time,
-                senderLocation: senderLatLon,
-                receiverLocation: receiverLatLon,
-            });
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(500).send(error);
+        res.status(200).send({
+            distance,
+            duration,
+            senderLocation,
+            receiverLocation,
         });
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error);
+    }
 });
 
-router.post('/send', (req, res) => {
+router.post('/send', async (req, res) => {
     const { userId, receiver, content, pigeonId } = req.body;
     console.log('POST send:', userId, receiver, content, pigeonId);
 
-    let sender;
+    try {
+        const { sender, duration } = await prepareSend(userId, receiver, pigeonId);
 
-    getUserRef(userId).get()
-        .then(userSnapshot => {
-            sender = userSnapshot.get('stationId');
-            // TODO: add this after adding async to fix returns
-            // if (sender === receiver) {
-            //     return res.status(400).send('cannot send message to self');
-            // }
-            return Promise.all([getStationRef(sender).get(), getStationRef(receiver).get()])
-        })
-        .then(([senderSnapshot, receiverSnapshot]) => {
-            if (!senderSnapshot.exists) {
-                return res.status(400).send('sender station does not exist');
-            } else if (!receiverSnapshot.exists) {
-                return res.status(400).send('receiver station does not exist');
-            }
-            const senderLocation = senderSnapshot.get('location');
-            const receiverLocation = receiverSnapshot.get('location');
+        const timestamp = new firestore.Timestamp(Math.floor(Date.now() / 1000), 0);
+        const messageData = {
+            sender,
+            receiver,
+            content,
+            timestamp,
+            isSent: false,
+        };
 
-            const senderLatLon = {
-                lat: senderLocation.latitude,
-                lon: senderLocation.longitude,
-            };
-            const receiverLatLon = {
-                lat: receiverLocation.latitude,
-                lon: receiverLocation.longitude,
-            }
+        const newMessageRef = messagesRef.doc();
+        setTimeout(() => {
+            newMessageRef.update({
+                isSent: true,
+            });
+        }, duration * 1000);
 
-            const distance = getDistanceFromLatLonInKm(
-                senderLatLon.lat, senderLatLon.lon,
-                receiverLatLon.lat, receiverLatLon.lon,
-            );
+        await newMessageRef.set(messageData);
 
-            const timestamp = new firestore.Timestamp(Math.floor(Date.now() / 1000), 0);
-            const messageData = {
-                sender,
-                receiver,
-                content,
-                timestamp,
-                isSent: false,
-            };
-
-            const duration = distance / PIGEON_SPEED * 60 * 60; // seconds
-
-            const newMessageRef = messagesRef.doc();
-            setTimeout(() => {
-                newMessageRef.update({
-                    isSent: true,
-                });
-            }, duration * 1000);
-
-            newMessageRef.set(messageData);
-
-            return newMessageRef;
-        })
-        .then(message => {
-            res.status(200).send(message.id);
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(500).send(error);
-        });
+        res.status(200).send(newMessageRef.id);
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error);
+    }
 });
 
 export default router;
