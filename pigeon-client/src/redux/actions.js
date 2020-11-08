13b1467738fd0df6a35236f4d2ef1c682ac66df2
Patrick Zhao <patrick123cool@gmail.com>
2020-11-07 23:58:06 -0800

    add MESSAGE_LIST to redux, create skeleton for MessageView

--- pigeon-client/src/redux/actions.js
+++ pigeon-client/src/redux/actions.js
@@ -1,8 +1,15 @@
-import { SET_USER_ID } from './actionTypes';
+import { SET_MESSAGE_LIST, SET_USER_ID } from './actionTypes';
 
 export const setUserId = userId => ({
   type: SET_USER_ID,
   payload: {
     userId,
+  },
+});
+
+export const setMessageList = messageList => ({
+  type: SET_MESSAGE_LIST,
+  payload: {
+    messageList,
   },
 });
